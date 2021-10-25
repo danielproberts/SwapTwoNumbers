838a852af744b2c695e64abae6817e181e31af43
Daniel Roberts <daniel.prescott.roberts@gmail.com>
2021-10-25 18:39:44 -0400

    Add project files.

---
+++ SwapTwoNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SwapTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int val1 = 2;
+            int val2 = 5;
+            Console.WriteLine("Val1 = " + val1);
+            Console.WriteLine("Val2 = " + val2);
+            Swap(ref val1, ref val2);
+
+            Console.WriteLine();
+            Console.WriteLine("After Swap: ");
+            Console.WriteLine("Val1 = " + val1);
+            Console.WriteLine("Val2 = " + val2);
+
+            Console.ReadKey();
+        }
+
+        static void Swap(ref int num1, ref int num2)
+        {
+            int tempNum;
+            tempNum = num1;
+            num1 = num2;
+            num2 = tempNum;
+        }
+    }
+}
